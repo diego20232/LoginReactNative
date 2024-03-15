682430c82679582ae005fb35df7304930ceb5181
diego20232 <diegosampaio986@gmail.com>
2024-03-15 11:26:22 -0300

    login react-native

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { TextInput } from "react-native-paper";
+import { Image, StyleSheet, Text, View } from 'react-native';
+import Login from "./components/Login";
+
+
+export default () => {
+   const [nome, setNome] = useState('');
+
+
+
+return (
+    <View style={[style.centro, style.fundo]}>
+        <Image source={require('./imagens/icons.png')}/>
+        <Text style={style.texto}>Tela de Cadastro</Text>
+        <Login/>
+    </View>
+)
+}
+
+
+const style = StyleSheet.create(
+    {
+        centro: {
+            alignItems: 'center',
+            padding: 50,
+        },
+        texto: {
+            fontSize: 23,
+            padding: 40,
+            color: 'white',
+            fontFamily: 'arial'
+        },
+        fundo: {
+            flex: 1,
+            backgroundColor: '#2F4F4F',
+        }
+    }
+)
---
+++ src/components/Login.jsx
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import { Button, IconButton, Text, TextInput } from "react-native-paper";
+import { StyleSheet, View } from "react-native";
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Image } from "react-native-paper/lib/typescript/components/Avatar/Avatar";
+
+
+export default () => {
+    const [nome, setNome] = useState('');
+    const [senha, setSenha] = useState('');
+    const [msg, setMsg] = useState('');
+
+// const [senha, setSenha] = useState('');
+// const [msglogin, setMsgLogin] = useState('');
+
+// const senhaUsuario = 'senha123';
+
+
+const clique = () => {
+    if(!nome.trim()) {
+        setMsg('insira seu nome de usúario');
+    }else if(senha !=='senha123') {
+        setMsg('senha Incorreta')
+    }else {
+        setMsg(`Bem vindo ao sistema ${nome}`)
+    }
+         
+    
+}
+
+// const cliqueSenha = () => {
+//     if(no === senhaCorreta) {
+//         setMsgLogin(`Sucesso no login`)
+//     }else  
+//         setMsgLogin(`senha errada`)
+// };
+
+
+
+    return (
+        <View>
+            <Text style={{fontSize: 15, color: 'white', textAlign:'center', padding: 5}}>Digite seu nome de usúario</Text>
+            <TextInput style={estilo.input}keyboardType="email-address" maxLength={50} onChangeText={text => setNome(text)} value={nome} placeholder="Exemplo: Diego Sampaio"/>
+            <Text style={{fontSize: 15, color: 'white', textAlign:'center', padding: 5}}>Digite sua senha!</Text>
+            <TextInput style={estilo.input} keyboardType="numeric" secureTextEntry={true} maxLength={20} label="password" onChangeText={text => setSenha(text)} value={senha}/>
+
+            <Button icon={require('../assets/fonts/icon.png')} style={{
+                width: 300,
+                margin: 15,
+                height: 40,
+            }} mode="contained" buttonColor="#5F9EA0" textColor="white"  onPress={clique} on>Entrar</Button>
+
+            <Text style={{
+                fontSize: 20,
+                color: 'white',
+                textAlign: 'center',
+                fontWeight: "bold",
+            }}>{msg}</Text>
+
+            {/* <Button icon={require('../assets/fonts/saida.png')} style={{
+                width: 300,
+                margin: 13,
+                height: 40,
+                
+            }} mode="contained" buttonColor="#5F9EA0" textColor="white" onPress={cliqueSenha}>Sair</Button>
+            <Text style={{
+                fontSize: 20,
+                color: 'white',
+                textAlign: 'center',
+            }}>{msglogin}</Text> */}
+
+
+        </View>
+    )
+}
+
+
+const estilo = StyleSheet.create(
+    {
+        input: {
+            borderCurve: 'circular',
+            // borderRadius: 25,
+            borderBottomWidth: 1,
+        },
+        padding: {
+            padding: 10,
+        }
+    }
+)
